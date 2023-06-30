4afb8ca4de956f5064d903335fa800990796b6fa
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-06-30 11:02:50 -0600

    created dto

---
+++ DTO/Conditions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Conditions
+    {
+        public Conditions() { }
+        public int comission { get; set; }
+        public double yearlyInterest { get; set; }
+        //interes adicional
+        public double addYearlyInterest { get; set;}
+    }
+}
---
+++ DTO/Loan.cs
@@ -0,0 +1,10 @@
+﻿namespace DTO
+{
+    public class Loan
+    {
+        // 0 - 2 hipoteca, personal,vehicular
+        public int valor { get; set; }
+        public string tipo { get; set; }
+        public int moneda { get; set; } 
+    }
+}
